994d3764e57b43381932744290f6a5fa0ea7f10e
ManiKumar Nakka <manikumarnakka324@gmail.com>
2025-09-10 07:14:47 +0530

    metadata added

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
      tailwindcss(),
-    react()
+    react(),
   ],
+  base : process.env.VITE_BASE_PATH || '/leo9-hero/tree/master',
 })
